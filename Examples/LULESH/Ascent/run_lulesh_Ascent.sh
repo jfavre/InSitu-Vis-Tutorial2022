86958614bf3bd43752897c074f0fd167df529d91
Jean M Favre <jfavre@cscs.ch>
2023-05-12 11:06:28 +0200

    lulesh Piz Daint demo

--- Examples/LULESH/Ascent/run_lulesh_Ascent.sh
+++ Examples/LULESH/Ascent/run_lulesh_Ascent.sh
@@ -3,18 +3,17 @@
 #SBATCH --account="csstaff"
 ##SBATCH --reservation="in-situ"
 #SBATCH --time=00:10:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-core=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=debug
 #SBATCH --constraint=gpu
 #SBATCH --hint=nomultithread
 
-module load daint-gpu Ascent
+# should run one task per node becasue Asent needs a dedicated CUDA node per task
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export VTK_SILENCE_GET_VOID_POINTER_WARNINGS=1
 
 mkdir $SCRATCH/Lulesh
 
@@ -24,4 +23,4 @@ cp ../buildAscent/bin/lulesh2.0 $SCRATCH/Lulesh
 # cp ascent_actions.yaml trigger_ascent_actions.yaml  $SCRATCH/Lulesh
 
 pushd $SCRATCH/Lulesh
-srun ./lulesh2.0 -s 30 -p
+srun ./lulesh2.0 -s 30 -p -i 1000
