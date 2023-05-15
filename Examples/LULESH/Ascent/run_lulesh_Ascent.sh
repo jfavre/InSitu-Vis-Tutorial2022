226280e2ac73ccdf3169ed158c9b6a3fc42abda6
Jean M Favre <jfavre@cscs.ch>
2023-05-15 11:09:18 +0200

    LULESH+Ascent new exercise misc

--- Examples/LULESH/Ascent/run_lulesh_Ascent.sh
+++ Examples/LULESH/Ascent/run_lulesh_Ascent.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --job-name="lulesh+ascent"
-#SBATCH --account="csstaff"
+##SBATCH --account="csstaff"
 ##SBATCH --reservation="in-situ"
 #SBATCH --time=00:10:00
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=12
 #SBATCH --partition=debug
 #SBATCH --constraint=gpu
 #SBATCH --hint=nomultithread
@@ -20,7 +20,8 @@ mkdir $SCRATCH/Lulesh
 cp ../buildAscent/bin/lulesh2.0 $SCRATCH/Lulesh
 
 # enable a custom scene description
-# cp ascent_actions.yaml trigger_ascent_actions.yaml  $SCRATCH/Lulesh
+cp ascent_actions.yaml trigger_ascent_actions.yaml  $SCRATCH/Lulesh
+#cp trigger_isocontours_actions.yaml  $SCRATCH/Lulesh
 
 pushd $SCRATCH/Lulesh
 srun ./lulesh2.0 -s 30 -p -i 1000
