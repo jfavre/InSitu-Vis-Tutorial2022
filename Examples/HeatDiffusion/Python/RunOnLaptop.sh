b38536dd72779f435f2ad21f5dfc332200621871
Jean M. Favre <jfavre@cscs.ch>
2023-10-06 14:28:57 +0200

    update startup files

--- Examples/HeatDiffusion/Python/RunOnLaptop.sh
+++
@@ -1,7 +0,0 @@
-
-export PYTHONPATH=/local/apps/Ascent/install/conduit-v0.8.7/python-modules:/local/apps/Ascent/install/ascent-v0.9.1/python-modules:$PYTHONPATH
-export LD_LIBRARY_PATH=/local/apps/Ascent/install/ascent-v0.9.1/lib:/local/apps/Ascent/install/vtk-m-v1.9.0/lib
-
-rm temperature-ser.0*png Temperature-iso-contours.01.png 
-python3 jacobi_insitu_Ascent.py
-
